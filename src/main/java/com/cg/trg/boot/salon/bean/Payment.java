3e76ea78ceca506c973559f5329eb5290f08e33f
Phantomblu3 <sulabh21198@gmail.com>
2021-04-08 16:16:20 +0530

    First github Push
    First github Push

--- src/main/java/com/cg/trg/boot/salon/bean/Payment.java
+++ src/main/java/com/cg/trg/boot/salon/bean/Payment.java
@@ -1,8 +1,47 @@
 package com.cg.trg.boot.salon.bean;
 
+import javax.persistence.*;
 public class Payment {
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long paymentId;
 	private String type;
 	private String status;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "paymentId" , referencedColumnName="cardId")
 	private Card card;
+	}
+
+	public void setPaymentId(long paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [paymentId=" + paymentId + ", type=" + type + ", status=" + status + ", card=" + card + "]";
+	}
+
 }
