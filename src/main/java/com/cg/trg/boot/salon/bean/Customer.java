2af755822f064d205332f49fcf0670934077814a
mdkhurshidalamgithub <kursed316@gmail.com>
2021-04-08 18:19:02 +0530

    up

--- src/main/java/com/cg/trg/boot/salon/bean/Customer.java
+++ src/main/java/com/cg/trg/boot/salon/bean/Customer.java
@@ -1,17 +1,108 @@
 package com.cg.trg.boot.salon.bean;
 
 import java.time.LocalDate;
+import java.util.Set;
 
-import javax.persistence.OneToMany;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+@Entity
+@Table(name = "Customer")
 public class Customer {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String userId;
 	private String name;
 	private String email;
 	private String contactNo;
 	private  LocalDate dob;
-	@OneToMany(mappedBy = "customer")
-	private Address address;
+	@ManyToMany
+	@JoinTable(
+			name = "customer_address",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "address_id")
+			)
+	private Set<Address> addresses;
+	
+	public Customer() {
+		
+	}
+
+	
+	public Customer(String userId, String name, String email, String contactNo, LocalDate dob, Set<Address> addresses) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.contactNo = contactNo;
+		this.dob = dob;
+		this.addresses = addresses;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	
+
+	public Set<Address> getAddresses() {
+		return addresses;
+	}
+
+
+	public void setAddresses(Set<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [userId=" + userId + ", name=" + name + ", email=" + email + ", contactNo=" + contactNo
+				+ ", dob=" + dob + ", address=" + addresses+ "]";
+	}
+	
+	
 
 }
