7405ff173d9c1bc6f9d20378df6948c721c25013
mdkhurshidalamgithub <kursed316@gmail.com>
2021-04-08 18:37:24 +0530

    update address

--- src/main/java/com/cg/trg/boot/salon/bean/Address.java
+++ src/main/java/com/cg/trg/boot/salon/bean/Address.java
@@ -1,5 +1,7 @@
 package com.cg.trg.boot.salon.bean;
 
+import java.util.Set;
+
 public class Address {
 
 	private String doorNo;
@@ -8,4 +10,65 @@ public class Address {
 	private String city;
 	private String state;
 	private int pincode;
+	private Set<Customer>customers;
+	public Address(String doorNo, String street, String area, String city, String state, int pincode,
+			Set<Customer> customers) {
+		super();
+		this.doorNo = doorNo;
+		this.street = street;
+		this.area = area;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+		this.customers = customers;
+	}
+	public String getDoorNo() {
+		return doorNo;
+	}
+	public void setDoorNo(String doorNo) {
+		this.doorNo = doorNo;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	public Set<Customer> getCustomers() {
+		return customers;
+	}
+	public void setCustomers(Set<Customer> customers) {
+		this.customers = customers;
+	}
+	@Override
+	public String toString() {
+		return "Address [doorNo=" + doorNo + ", street=" + street + ", area=" + area + ", city=" + city + ", state="
+				+ state + ", pincode=" + pincode + ", customers=" + customers + "]";
+	}
+	
+	
 }
