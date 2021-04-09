5d1de2a9d8c9e2af4b376e5d53655bf689957107
Phantomblu3 <sulabh21198@gmail.com>
2021-04-09 18:31:37 +0530

    paymentService Impl

--- src/main/java/com/cg/trg/boot/salon/dao/IPaymentRepository.java
+++ src/main/java/com/cg/trg/boot/salon/dao/IPaymentRepository.java
@@ -2,9 +2,11 @@ package com.cg.trg.boot.salon.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.trg.boot.salon.bean.Payment;
 
-public interface IPaymentRepository {
+public interface IPaymentRepository extends JpaRepository<Payment, Long> {
 	public Payment addPayment(Payment payment);
 	public Payment removePayment(long id);
 	public Payment updatePayment(long id, Payment payment);
--- src/main/java/com/cg/trg/boot/salon/service/IPaymentService.java
+++ src/main/java/com/cg/trg/boot/salon/service/IPaymentService.java
@@ -2,12 +2,14 @@ package com.cg.trg.boot.salon.service;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.trg.boot.salon.bean.Payment;
 
 public interface IPaymentService {
 	public Payment addPayment(Payment payment);
-	public Payment removePayment(long id);
-	public Payment updatePayment(long id, Payment payment);
-	public Payment getPaymentDetails(long id);
+	public Payment removePayment(long paymentId);
+	public Payment updatePayment(long paymentId, Payment payment);
+	public Payment getPaymentDetails(long paymentId);
 	public List<Payment> getAllPaymentDetails();
 }
