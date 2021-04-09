e99a237e5f823f796914598697f474fa1a19ab06
Ankit Dutta <46992030+ankitdutta170@users.noreply.github.com>
2021-04-09 21:18:37 +0530

    Modifying for running the application

--- src/main/java/com/cg/trg/boot/salon/dao/IPaymentRepository.java
+++ src/main/java/com/cg/trg/boot/salon/dao/IPaymentRepository.java
@@ -8,17 +8,13 @@ import com.cg.trg.boot.salon.bean.Payment;
 
 
 public interface IPaymentRepository extends JpaRepository<Payment, Long> {
-	public Payment addPayment(Payment payment);
-	public Payment removePayment(long id);
-	public Payment updatePayment(long id, Payment payment);
-	public Payment getPaymentDetails(long id);
-	public List<Payment> getAllPaymentDetails();
-
-}
 	/*
 	 * public Payment addPayment(Payment payment); public Payment removePayment(long
 	 * id); public Payment updatePayment(long id, Payment payment); public Payment
 	 * getPaymentDetails(long id); public List<Payment> getAllPaymentDetails();
 	 */
+
+}
+	
 
 
