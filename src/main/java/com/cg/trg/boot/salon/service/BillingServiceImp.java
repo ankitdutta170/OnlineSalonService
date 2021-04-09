df268741f2f8a32004120e43a319bd7f141753a6
omprakashsahoo-tech <81797242+omprakashsahoo-tech@users.noreply.github.com>
2021-04-09 16:59:14 +0530

    Update BillingServiceImp.java

--- src/main/java/com/cg/trg/boot/salon/service/BillingServiceImp.java
+++ src/main/java/com/cg/trg/boot/salon/service/BillingServiceImp.java
@@ -1,5 +1,194 @@
 package com.cg.trg.boot.salon.service;
 
-public class BillingServiceImp {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.cg.trg.boot.salon.bean.Billing;
+import com.cg.trg.boot.salon.dao.IBillingRepository;
+@Service
+public class BillingServiceImp implements IBillingService {
+	@Autowired
+	IBillingRepository repository;
+	@Override
+	public List<Billing> findAll() {
+		// TODO Auto-generated method stub	
+		 List<Billing> repository.getAllOrders();
+		return ;
+	}
+
+	@Override
+	public List<Billing> findAll(Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Billing> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Billing> List<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Billing> S saveAndFlush(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteInBatch(Iterable<Billing> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Billing getOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Billing> List<S> findAll(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Billing> List<S> findAll(Example<S> example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Billing> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Billing> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Billing> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Billing entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Billing> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Billing> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Billing> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Billing> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends Billing> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Billing addBill(Billing bill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Billing removeBill(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Billing updateBill(long id, Billing bill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Billing getBillDetails(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Billing> getAllBills() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
