612a818660ab8fba91f7bbd02d2b9d59d6b600c6
SIDDHARTH DAS <dassiddharth34@hotmail.com>
2021-04-10 11:31:57 +0530

    USER SERVICE & CONTROLLER

--- src/main/java/com/cg/trg/boot/salon/dao/IUserRepository.java
+++ src/main/java/com/cg/trg/boot/salon/dao/IUserRepository.java
@@ -1,10 +1,17 @@
 package com.cg.trg.boot.salon.dao;
 
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.trg.boot.salon.bean.User;
 
-public interface IUserRepository {
+public interface IUserRepository extends JpaRepository<User, Long>{
 	public User signIn(User user);
 	public User signOut(User user);
 	public User changePassword(long id, User user);
+	
 
 }
--- src/main/java/com/cg/trg/boot/salon/service/IUserService.java
+++ src/main/java/com/cg/trg/boot/salon/service/IUserService.java
@@ -5,6 +5,7 @@ import com.cg.trg.boot.salon.bean.User;
 public interface IUserService {
 	public User signIn(User user);
 	public User signOut(User user);
-	public User changePassword(long id, User user);
+	public User changePassword(long id, String changedPassword);
+	
 
 }
