50bfdcee665d9efeef42642adb73239aeeffdd49
omprakashsahoo-tech <81797242+omprakashsahoo-tech@users.noreply.github.com>
2021-04-10 14:34:39 +0530

    update

--- src/main/java/com/cg/trg/boot/salon/bean/Customer.java
+++ src/main/java/com/cg/trg/boot/salon/bean/Customer.java
@@ -28,10 +28,7 @@ public class Customer extends User {
 	@OneToMany(mappedBy = "customer")
 	private List<Billing> bills;
 	
-	
-	
-	
-	
+		
 
 	public Customer(String userId, String password, String role, boolean isLoggedIn) {
 		super(userId, password, role, isLoggedIn);
@@ -52,20 +49,6 @@ public class Customer extends User {
 		this.appointment = appointment;
 		this.bills = bills;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	public String getName() {
