5ff3090e8151b5198ae8ef0dd199a60b8aa8fb58
sai praveen <fandlatpraveen@gmail.com>
2021-04-10 10:41:44 +0530

    saloncontroller and exception updated

---
+++ src/main/java/com/cg/trg/boot/salon/controllers/SalonServiceController.java
@@ -0,0 +1,79 @@
+package com.cg.trg.boot.salon.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.cg.trg.boot.salon.bean.SalonService;
+import com.cg.trg.boot.salon.exceptions.AppointmentNotFoundException;
+import com.cg.trg.boot.salon.exceptions.EmptyDataException;
+import com.cg.trg.boot.salon.exceptions.NoDataException;
+import com.cg.trg.boot.salon.exceptions.SalonServiceNotFoundException;
+import com.cg.trg.boot.salon.service.ISalonServiceImpl;
+
+@RestController
+@RequestMapping("salonservices")
+public class SalonServiceController {
+
+	@Autowired
+    ISalonServiceImpl service;
+
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String saveSalonService(SalonService salonservice) {
+		SalonService saveSalonService = service.addService(salonservice);
+		if(saveSalonService != null) {
+			return "Service successfully added";
+		}
+		else
+			return "Failed to add service";
+	}
+	
+	@GetMapping("{aid}")
+	public ResponseEntity<?> getSalonService(@PathVariable("aid")long id){
+		SalonService service1 = service.getService(id);
+		if(service1 == null) {
+			throw new AppointmentNotFoundException("Request", "Salon Service with appointment id:"+id+"not found");
+		}
+		return new ResponseEntity<SalonService>(service1, HttpStatus.OK);
+	}
+	@GetMapping
+	public List<SalonService> getAllServices(){
+		List<SalonService>services = service.getAllServices();
+		if(services.size() == 0) {
+			throw new EmptyDataException("No Service saved in database");
+		}
+		return services;
+		
+	}
+    
+    @PutMapping
+	public String updateSalonService(long id, SalonService salonservice) {
+		SalonService updatedSalonService = service.updateService(id, salonservice);
+		if(updatedSalonService != null) {
+			return "SalonService succesfully updated";
+		}
+		else
+			return "SalonService failed to update";
+	}
+    @DeleteMapping("{aid}")
+	public String removeSalonService(@PathVariable("aid") long id) {
+		SalonService deleteSalonService = service.removeService(id);
+		if(deleteSalonService != null) {
+			return "SalonService successfully deleted";
+		}
+		else
+			return "SalonService failed to delete";
+	}
+}
---
+++ src/main/java/com/cg/trg/boot/salon/exceptions/NoDataException.java
@@ -0,0 +1,12 @@
+package com.cg.trg.boot.salon.exceptions;
+
+
+
+public class NoDataException extends RuntimeException {
+	
+	public NoDataException(String message) {
+		super(message);
+	}
+
+
+}
