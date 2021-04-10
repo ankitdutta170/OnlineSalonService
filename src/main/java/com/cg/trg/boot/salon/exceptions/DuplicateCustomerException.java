e159e60db7cef6211fd92d7c2840afea8a9440db
omprakashsahoo-tech <81797242+omprakashsahoo-tech@users.noreply.github.com>
2021-04-10 23:05:26 +0530

    update

--- src/main/java/com/cg/trg/boot/salon/exceptions/DuplicateCustomerException.java
+++
@@ -1,8 +0,0 @@
-package com.cg.trg.boot.salon.exceptions;
-
-public class DuplicateCustomerException extends Exception {
-	public DuplicateCustomerException(String message) {
-		super(message);
-	}
-
-}
