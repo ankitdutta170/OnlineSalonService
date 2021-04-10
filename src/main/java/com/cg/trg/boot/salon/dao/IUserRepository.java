5dbe9eec891dab4bfb4878074ac116f99640f441
ankitdutta170 <ankitdutta170@gmail.com>
2021-04-10 14:00:26 +0530

    Revert "Service impl"
    This reverts commit 78c011931e4b3e5e5313b4d980bb4f3f473c8507.

--- src/main/java/com/cg/trg/boot/salon/dao/IUserRepository.java
+++ src/main/java/com/cg/trg/boot/salon/dao/IUserRepository.java
@@ -1,21 +1,14 @@
 package com.cg.trg.boot.salon.dao;
 
-<<<<<<< Updated upstream
 
 
 
 
-=======
->>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cg.trg.boot.salon.bean.User;
 
-<<<<<<< Updated upstream
 public interface IUserRepository extends JpaRepository<User, Long>{
-=======
-public interface IUserRepository extends JpaRepository<User, Long> {
->>>>>>> Stashed changes
 	public User signIn(User user);
 	public User signOut(User user);
 	public User changePassword(long id, User user);
--- src/main/java/com/cg/trg/boot/salon/service/IUserService.java
+++ src/main/java/com/cg/trg/boot/salon/service/IUserService.java
@@ -1,12 +1,8 @@
 package com.cg.trg.boot.salon.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.cg.trg.boot.salon.bean.User;
 
-public interface IUserService  {
-	
-	
+public interface IUserService {
 	public User signIn(User user);
 	public User signOut(User user);
 	public User changePassword(long id, String changedPassword);
