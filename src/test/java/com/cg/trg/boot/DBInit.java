3815a7fbbb6662085312cfb354c846dab7974a58
ankitdutta170 <ankitdutta170@gmail.com>
2021-04-14 09:40:08 +0530

    Implementing h2 database

---
+++ src/test/java/com/cg/trg/boot/DBInit.java
@@ -0,0 +1,34 @@
+package com.cg.trg.boot;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import com.cg.trg.boot.salon.bean.Address;
+import com.cg.trg.boot.salon.bean.Appointment;
+import com.cg.trg.boot.salon.bean.Billing;
+import com.cg.trg.boot.salon.bean.Customer;
+import com.cg.trg.boot.salon.dao.ICustomerRepository;
+
+
+
+public class DBInit implements CommandLineRunner{
+	@Autowired
+	ICustomerRepository customerRepository;
+	org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DBInit.class);
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Data  Entry process initiated for Customer table");
+		customerRepository.save(new Customer("Ankit","abc@gmail.com","7903083839",LocalDate.of(1999, 4, 8),new ArrayList<Billing>(),new HashSet<Address>(),new ArrayList<Appointment>()));
+		customerRepository.save(new Customer("Sai","abcd@gmail.com","7903083838",LocalDate.of(1999, 4, 7),new ArrayList<Billing>(),new HashSet<Address>(),new ArrayList<Appointment>()));
+		customerRepository.save(new Customer("Om","abcde@gmail.com","7903083837",LocalDate.of(1999, 4, 6),new ArrayList<Billing>(),new HashSet<Address>(),new ArrayList<Appointment>()));
+		customerRepository.save(new Customer("Sulabh","abcdef@gmail.com","7903083836",LocalDate.of(1999, 4, 5),new ArrayList<Billing>(),new HashSet<Address>(),new ArrayList<Appointment>()));
+		customerRepository.save(new Customer("Siddharth","abcdefg@gmail.com","7903083835",LocalDate.of(1999, 4, 4),new ArrayList<Billing>(),new HashSet<Address>(),new ArrayList<Appointment>()));
+		customerRepository.save(new Customer("Kurshed","abcdefgh@gmail.com","7903083834",LocalDate.of(1999, 4, 3s),new ArrayList<Billing>(),new HashSet<Address>(),new ArrayList<Appointment>()));
+		
+	}
+
+}
