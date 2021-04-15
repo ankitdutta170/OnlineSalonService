c8eee1516ec9311158a9f865ceeac0f18a8492e9
omprakashsahoo-tech <81797242+omprakashsahoo-tech@users.noreply.github.com>
2021-04-15 13:10:46 +0530

    commit

--- src/main/java/com/cg/trg/boot/salon/controllers/SalonServiceController.java
+++ src/main/java/com/cg/trg/boot/salon/controllers/SalonServiceController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import com.cg.trg.boot.salon.bean.SalonService;
 import com.cg.trg.boot.salon.exceptions.NoDataException;
 import com.cg.trg.boot.salon.exceptions.SalonServiceNotFoundException;
 import com.cg.trg.boot.salon.service.ISalonServiceImpl;
-
+@CrossOrigin
 @RestController
 @RequestMapping("salonservices")
 public class SalonServiceController {
